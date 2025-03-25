d575a198d796f87a8536755471c0598642fb72d5
David Case <david.case@shruggr.cloud>
2025-03-25 22:20:21 +0000

    add context to topic manager functions

--- pkg/core/engine/topic-manager.go
+++ pkg/core/engine/topic-manager.go
@@ -1,14 +1,16 @@
 package engine
 
 import (
+	"context"
+
 	"github.com/bsv-blockchain/go-sdk/chainhash"
 	"github.com/bsv-blockchain/go-sdk/overlay"
 	"github.com/bsv-blockchain/go-sdk/transaction"
 )
 
 type TopicManager interface {
-	IdentifyAdmissableOutputs(beef *transaction.Beef, txid *chainhash.Hash, previousCoins []uint32) (overlay.AdmittanceInstructions, error)
-	IdentifyNeededInputs(beef *transaction.Beef, txid *chainhash.Hash) ([]*overlay.Outpoint, error)
+	IdentifyAdmissableOutputs(ctx context.Context, beef *transaction.Beef, txid *chainhash.Hash, previousCoins []uint32) (overlay.AdmittanceInstructions, error)
+	IdentifyNeededInputs(ctx context.Context, beef *transaction.Beef, txid *chainhash.Hash) ([]*overlay.Outpoint, error)
 	GetDocumentation() string
 	GetMetaData() *overlay.MetaData
 }
