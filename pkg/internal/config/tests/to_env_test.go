2a6c2c90fab9071c63a49b77f496d85d03352492
Augustyn Chmiel <augustyn.chmiel@acoustic.com>
2025-04-03 07:51:50 +0200

    review comments

--- pkg/internal/config/tests/to_env_test.go
+++ pkg/internal/config/tests/to_env_test.go
@@ -30,3 +30,25 @@ func TestToEnvFile(t *testing.T) {
 	require.Contains(t, content, `TEST_B_WITH_LONG_NAME="1"`)
 	require.Contains(t, content, `TEST_C_SUB_CONFIG_D_NESTED_FIELD="default_world"`)
 }
+
+func TestToEnvFile_WithEmptyPrefix(t *testing.T) {
+	// Given
+	tmpDir := t.TempDir()
+	configFilePath := fmt.Sprintf("%s/exported_config.env", tmpDir)
+	cfg := Defaults()
+
+	// When
+	err := config.ToEnvFile(cfg, configFilePath, "")
+
+	// Then
+	require.NoError(t, err)
+
+	data, err := os.ReadFile(configFilePath)
+	require.NoError(t, err)
+
+	content := string(data)
+
+	require.Contains(t, content, `A="default_hello"`)
+	require.Contains(t, content, `B_WITH_LONG_NAME="1"`)
+	require.NotContains(t, content, `_C_SUB_CONFIG_D_NESTED_FIELD="default_world"`)
+}
