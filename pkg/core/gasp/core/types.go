57181d9d8bd15935e22d4ce3b2dbe9ff160c2270
David Case <david.case@shruggr.cloud>
2025-06-17 16:12:13 -0400

    fix GASP JSON marshalling

--- pkg/core/gasp/core/types.go
+++ pkg/core/gasp/core/types.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/bsv-blockchain/go-sdk/transaction"
@@ -16,8 +17,116 @@ type GASPInitialResponse struct {
 	Since    uint32                  `json:"since"`
 }
 
+// MarshalJSON implements custom JSON marshalling for GASPInitialResponse
+func (g GASPInitialResponse) MarshalJSON() ([]byte, error) {
+	type OutpointObj struct {
+		Txid  string `json:"txid"`
+		Index uint32 `json:"index"`
+	}
+	
+	utxoList := make([]OutpointObj, len(g.UTXOList))
+	for i, outpoint := range g.UTXOList {
+		if outpoint != nil {
+			utxoList[i] = OutpointObj{
+				Txid:  outpoint.Txid.String(),
+				Index: outpoint.Index,
+			}
+		}
+	}
+	
+	return json.Marshal(&struct {
+		UTXOList []OutpointObj `json:"utxo_list"`
+		Since    uint32       `json:"since"`
+	}{
+		UTXOList: utxoList,
+		Since:    g.Since,
+	})
+}
+
+// UnmarshalJSON implements custom JSON unmarshalling for GASPInitialResponse
+func (g *GASPInitialResponse) UnmarshalJSON(data []byte) error {
+	type OutpointObj struct {
+		Txid  string `json:"txid"`
+		Index uint32 `json:"index"`
+	}
+	
+	aux := &struct {
+		UTXOList []OutpointObj `json:"utxo_list"`
+		Since    uint32         `json:"since"`
+	}{}
+	
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+	
+	g.Since = aux.Since
+	g.UTXOList = make([]*transaction.Outpoint, len(aux.UTXOList))
+	
+	for i, obj := range aux.UTXOList {
+		outpoint, err := transaction.OutpointFromString(fmt.Sprintf("%s.%d", obj.Txid, obj.Index))
+		if err != nil {
+			return err
+		}
+		g.UTXOList[i] = outpoint
+	}
+	
+	return nil
+}
+
 type GASPInitialReply struct {
 	UTXOList []*transaction.Outpoint `json:"utxo_list"`
+}
+
+// MarshalJSON implements custom JSON marshalling for GASPInitialReply
+func (g GASPInitialReply) MarshalJSON() ([]byte, error) {
+	type OutpointObj struct {
+		Txid  string `json:"txid"`
+		Index uint32 `json:"index"`
+	}
+	
+	utxoList := make([]OutpointObj, len(g.UTXOList))
+	for i, outpoint := range g.UTXOList {
+		if outpoint != nil {
+			utxoList[i] = OutpointObj{
+				Txid:  outpoint.Txid.String(),
+				Index: outpoint.Index,
+			}
+		}
+	}
+	
+	return json.Marshal(&struct {
+		UTXOList []OutpointObj `json:"utxo_list"`
+	}{
+		UTXOList: utxoList,
+	})
+}
+
+// UnmarshalJSON implements custom JSON unmarshalling for GASPInitialReply
+func (g *GASPInitialReply) UnmarshalJSON(data []byte) error {
+	type OutpointObj struct {
+		Txid  string `json:"txid"`
+		Index uint32 `json:"index"`
+	}
+	
+	aux := &struct {
+		UTXOList []OutpointObj `json:"utxo_list"`
+	}{}
+	
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+	
+	g.UTXOList = make([]*transaction.Outpoint, len(aux.UTXOList))
+	
+	for i, obj := range aux.UTXOList {
+		outpoint, err := transaction.OutpointFromString(fmt.Sprintf("%s.%d", obj.Txid, obj.Index))
+		if err != nil {
+			return err
+		}
+		g.UTXOList[i] = outpoint
+	}
+	
+	return nil
 }
 
 type GASPInput struct {
